5e6f8a8a01ac3f01226d92ed8536f1c7ace00a87
sleepy chezz. <147243219+JustCallMeChezz@users.noreply.github.com>
2024-11-04 18:59:36 +0800

    Add files via upload

---
+++ pfps/pfps.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    const profilePhoto = document.getElementById('pfps');
+    const photoArray = [
+        '/pfps/pfp1.jpg',
+        '/pfps/pfp2.jpg',
+        '/pfps/pfp3.jpg',
+        '/pfps/pfp4.jpg',
+        '/pfps/pfp5.jpg',
+        '/pfps/pfp6.jpg'
+
+    ];
+    let photoIndex = 0;
+
+    function changePhoto() {
+        setTimeout(() => {
+            photoIndex = (photoIndex + 1) % photoArray.length;
+            profilePhoto.src = photoArray[photoIndex];
+        }, 1000);
+    }
+
+    setInterval(changePhoto, 5000); 
+};
