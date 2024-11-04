b475fccc00146ee301e76962037eb4cd5728c704
sleepy chezz. <147243219+JustCallMeChezz@users.noreply.github.com>
2024-11-04 18:46:04 +0800

    Add files via upload

---
+++ scripts/vibrate.js
@@ -0,0 +1,12 @@
+const elementsToVibrate = ['.interest', '.games', '.social-media', '.clickable-word'];
+
+  elementsToVibrate.forEach(selector => {
+    const element = document.querySelector(selector);
+    if (element) {
+      element.addEventListener('click', function() {
+        if (navigator.vibrate) {
+          navigator.vibrate(200);
+        }
+      });
+    }
+  });
