8019e1cff534fe2055fb1465ed404d7553d03d78
sleepy chezz. <147243219+JustCallMeChezz@users.noreply.github.com>
2025-06-28 01:13:44 +0800

    Delete chatbot directory

--- chatbot/date.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-            let dateElement = document.getElementById('chat-date');
-            let today = new Date();
-            let options = { month: 'long', day: 'numeric' };
-            let currentDate = today.toLocaleDateString('en-US', options);
-            dateElement.textContent = currentDate;
-        });
--- chatbot/sidepanel.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-            document.getElementById("sidepanel").style.width = "200px";
-        }
-
-        function closeNav() {
-            document.getElementById("sidepanel").style.width = "0";
-        }
