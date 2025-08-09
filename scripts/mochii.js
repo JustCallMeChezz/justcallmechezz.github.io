7ed4385af5981550ac0f8cfcf4f5bb117575d1f6
sleepy chezz. <147243219+JustCallMeChezz@users.noreply.github.com>
2025-08-09 21:58:30 +0800

    Update mochii.js

--- scripts/mochii.js
+++ scripts/mochii.js
@@ -1,11 +1,11 @@
-const elementsToVibrate = ['.profile img', '.theme', '.projects','.ai', '.uhhhh','.social-media', '.clickable-word'];
+const elementsToVibrate = ['.profile img', '.theme', '.projectss','.ai', '.uhhhh','.social-media', '.clickable-word'];
 
   elementsToVibrate.forEach(selector => {
     const element = document.querySelector(selector);
     if (element) {
       element.addEventListener('click', function() {
         if (navigator.vibrate) {
-          navigator.vibrate(200);
+          navigator.vibrate(100);
         }
       });
     }
